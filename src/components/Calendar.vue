4ea53dd8431aadf44ff72941a7230966d641db73
julia_alex <julia.a@cronix.ms>
2019-05-29 23:13:28 +0300

    add calendar

---
+++ src/components/Calendar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div style="text-align: center">
+        <p>{{monthNow}}</p>
+        <p>{{yearNow}}</p>
+        <div class="item">
+            <span class="week" v-for="day in days"></span>
+            <p class="days" v-for="one in daysCount">{{one}}</p>
+        </div>
+
+        <button @click="nextMonth">Назад</button>
+        <button>Вперед</button>
+
+    </div>
+</template>
+<script>
+    import moment  from 'moment'
+    export default {
+        name: 'Calendar',
+        data () {
+            return {
+                days: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница",
+                    "Суббота", "Воскресенье"],
+                today:''
+            }
+        },
+        methods: {
+            moment() {
+                return moment().locale('ru');
+            },
+            nextMonth() {
+
+                    this.moment = this.moment(this.moment()).add(1, 'month');
+                console.log(this.moment)
+            }
+        },
+        computed: {
+            daysCount () {
+                return this.moment().daysInMonth()
+            },
+            monthNow () {
+                return this.moment().format('MMMM')
+            },
+            yearNow () {
+                return this.moment().format('YYYY')
+            }
+        },
+    }
+</script>
